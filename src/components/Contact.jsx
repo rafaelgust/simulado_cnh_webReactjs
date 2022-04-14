71872940c15e251647ace67fa3b4a7e86ab6321a
rafaelgust <rafael.appdevs@gmail.com>
2022-04-13 22:31:05 -0300

    contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from "react";
 class Contact extends React.Component{
     render(){
         return(
-            <div></div>
+            <div className="contactContainer"></div>
         );
     }
 }
--- src/components/Policy.jsx
+++ src/components/Policy.jsx
@@ -14,21 +14,21 @@ class Policy extends React.Component{
                 <p>Sites da web recebem Cookies e Web Beacons quando o usuário acessa as páginas, dados estes que são instalados automaticamente e originados dos próprios servidores que hospedam todo conteúdo e também de servidores de terceiros que operam este site.</p>
                 <p>Com o Cookie DART, o Google pode exibir anúncios com base nas visitas que o leitor fez a outros websites na Internet;</p>
                 <br />
-                <p>O "simulacnh.com.br" pode possuir ligações para outros sites, os quais, a nosso ver, podem conter informações / ferramentas úteis para os nossos visitantes. A nossa política de privacidade não é aplicada a sites de terceiros, pelo que, caso visite outro site a partir do nosso deverá ler a politica de privacidade do mesmo</p>
+                <p>O "simulacnh.com.br" e seu aplicativo pode possuir ligações para outros sites, os quais, a nosso ver, podem conter informações / ferramentas úteis para os nossos visitantes. A nossa política de privacidade não é aplicada a sites de terceiros, pelo que, caso visite outro site a partir do nosso deverá ler a politica de privacidade do mesmo</p>
                 <p>Não nos responsabilizamos pela política de privacidade ou conteúdo presente nesses mesmos sites.</p>
-                <p>O "simulacnh.com.br" declara que não utiliza e-mails para políticas de SPAM ou de envio de mensagens indesejadas.</p>
+                <p>O "simulacnh.com.br" e seu aplicativo declara que não utiliza e-mails para políticas de SPAM ou de envio de mensagens indesejadas.</p>
                 <br />
                 <h2>Termos de serviço</h2>
                 <p>O conteúdo disponibilizado neste site tem propósito exclusivamente informativo, não consistindo em recomendações financeiras, legais, fiscais, contábeis ou de qualquer outra natureza. A utilização deste material é de inteira responsabilidade do usuário.</p>
-                <p>O "simulacnh.com.br" poderá alterar a forma e o conteúdo, bem como suspender ou cancelar, a seu exclusivo critério, a qualquer tempo, quaisquer dos serviços, produtos, utilidades ou aplicações disponibilizados no site. E envidará seus melhores esforços no sentido de manter as informações e materiais contidos neste site, de forma organizada, atualizando e mantendo completos os conteúdos, sempre que possível.</p>
+                <p>O "simulacnh.com.br" e seu aplicativo poderá alterar a forma e o conteúdo, bem como suspender ou cancelar, a seu exclusivo critério, a qualquer tempo, quaisquer dos serviços, produtos, utilidades ou aplicações disponibilizados no site. E envidará seus melhores esforços no sentido de manter as informações e materiais contidos neste site, de forma organizada, atualizando e mantendo completos os conteúdos, sempre que possível.</p>
                 <p>As informações disponíveis no site poderão ser atualizadas ou modificadas periodicamente e não devem ser interpretadas como definitivas.</p>
                 <p>O site e seus serviços estão sujeitos a eventuais problemas de interrupção, falha técnica, ou indisponibilidade de funcionamento temporário.</p>
-                <p>O "simulacnh.com.br" se exime de qualquer responsabilidade pelos danos e prejuízos de toda natureza que possam decorrer da falta de disponibilidade ou de continuidade do funcionamento do site e dos serviços.</p>
-                <p>O "simulacnh.com.br" não poderão ser responsabilizados por qualquer perda ou dano, direto ou indireto, que decorram de:</p>
+                <p>O "simulacnh.com.br" e seu aplicativo se exime de qualquer responsabilidade pelos danos e prejuízos de toda natureza que possam decorrer da falta de disponibilidade ou de continuidade do funcionamento do site e dos serviços.</p>
+                <p>O "simulacnh.com.br" e seu aplicativo não poderão ser responsabilizados por qualquer perda ou dano, direto ou indireto, que decorram de:</p>
                 <strong> * qualquer decisão ou ação tomada pelo usuário ou terceiros, com base nestas informações.</strong><br/>
                 <strong> * qualquer imprecisão ou falta de complemento das informações, atrasos, interrupções, erros ou omissões no fornecimento dos dados contidos neste site.</strong><br />
                 <br />
-                <p>"simulacnh.com.br" possui caráter meramente informativo, e não possui ligação com nenhum órgão público.</p>
+                <p>"simulacnh.com.br" e seu aplicativo possui caráter meramente informativo, e não possui ligação com nenhum órgão público.</p>
             </div>
         );
     }
